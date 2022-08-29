d432295b39018c8e93e6b12120825a153b5c5b51
Rfontt <ritafontenele@ext.cvccorp.com.br>
2022-08-29 18:46:48 -0300

    Finalizing duplex content

--- 04-transform-vs-duplex.js
+++ 04-transform-vs-duplex.js
@@ -1,4 +1,4 @@
-import { Duplex } from 'stream';
+import { Duplex, Transform } from 'stream';
 
 let count = 0;
 
@@ -39,5 +39,26 @@ server.write(`[duplex] hey this is a writable \n`);
 // on data show what happens in the push.
 server.on('data', msg => console.log(`[readable] ${msg}`))
 
+// The push lets you send more data.
+server.push(`[duplex] hey this is also a readable \n`);
+
 // server
 //     .pipe(process.stdout);
+
+const transformToUpperCase = Transform({
+    objectMode: true,
+    transform(chuck, enconding, cb) {
+        cb(null, chuck.toUpperCase());
+    }
+});
+
+// Transform is also a duplex, but don't have independent communication 
+transformToUpperCase.write('[transform] hello from write');
+
+// The push will ignore what you have in transform function.
+transformToUpperCase.push('[transform] hello from write \n');
+
+// Redirect all data from readable to writable from duplex
+server
+    .pipe(transformToUpperCase)
+    .pipe(server);
